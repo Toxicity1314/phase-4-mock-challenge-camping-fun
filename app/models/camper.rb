62902b9d1af7c9ee215293198e75839f0b88aaef
Sean Welsh <s.welsh1314@gmail.com>
2023-02-03 16:57:09 -0500

    i think i got it

---
+++ app/models/camper.rb
@@ -0,0 +1,8 @@
+class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups
+    
+    validates :name, presence: true
+    validates :age, numericality: { less_than:18, greater_than:8 }
+
+end
